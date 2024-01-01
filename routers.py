623f405e1a3732f30a9779af3f2dd6fce6e2dcc2
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-01-01 23:13:31 +0200

    adding movie endpoint

--- routers.py
+++ routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from view.movie import movieRouter
+from view.movie.movieRouter import movie_router
 
 main_api_router = APIRouter(prefix='/v1')
 
-main_api_router.include_router(movieRouter, prefix="movie")
+main_api_router.include_router(movie_router, prefix="/movie")
--- view/movie/movieRouter.py
+++ view/movie/movieRouter.py
@@ -2,3 +2,8 @@ from other.parse_movies import parse_movies
 
 from fastapi import APIRouter
 
+movie_router = APIRouter()
+
+@movie_router.get("/movies")
+async def movie(year: int):
+    return parse_movies(year)
